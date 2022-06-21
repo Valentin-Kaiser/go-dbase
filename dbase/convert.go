1f2ee8d93487fa2ae88983896e715a127f31a23e
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2022-06-21 10:02:11 +0200

    [FIX] merged convert and decoder package

--- dbase/convert.go
+++ dbase/convert.go
@@ -1,13 +1,19 @@
 package dbase
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"strconv"
 	"strings"
 	"time"
 	"unicode"
+	"unicode/utf8"
+
+	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/transform"
 )
 
 // convert year, month and day to a julian day number
@@ -174,11 +180,45 @@ func (dbf *DBF) parseFloat(raw []byte) (float64, error) {
 	return strconv.ParseFloat(strings.TrimSpace(string(trimmed)), 64)
 }
 
-// toUTF8String converts a byte slice to a UTF8 string using the decoder in dbf
+// toUTF8String converts a byte slice to a UTF8 string using the converter
 func (dbf *DBF) toUTF8String(raw []byte) (string, error) {
-	utf8, err := dbf.decoder.Decode(raw)
+	utf8, err := dbf.convert.Decode(raw)
 	if err != nil {
 		return string(raw), fmt.Errorf("dbase-reader-to-utf8-string-1:FAILED:%v", err)
 	}
 	return string(utf8), nil
+}
+
+// Decoder is the interface as passed to OpenFile
+type Converter interface {
+	Decode(in []byte) ([]byte, error)
+	Encode(in []byte) ([]byte, error)
+}
+
+// Win1250Decoder translates a Windows-1250 DBF to UTF8 and back
+type Win1250Converter struct{}
+
+// Decode decodes a Windows1250 byte slice to a UTF8 byte slice
+func (d *Win1250Converter) Decode(in []byte) ([]byte, error) {
+	if utf8.Valid(in) {
+		return in, nil
+	}
+	r := transform.NewReader(bytes.NewReader(in), charmap.Windows1250.NewDecoder())
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+// Decode decodes a UTF8 byte slice to a Windows1250 byte slice
+func (d *Win1250Converter) Encode(in []byte) ([]byte, error) {
+	out := make([]byte, len(in))
+	enc := charmap.Windows1250.NewEncoder()
+	nDst, _, err := enc.Transform(out, in, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return out[:nDst], nil
 }
--- dbase/convert_test.go
+++ dbase/convert_test.go
@@ -97,3 +97,31 @@ func TestToBool(t *testing.T) {
 		t.Error("Want false")
 	}
 }
+
+func TestWin1250Converter_Decode(t *testing.T) {
+	dec := new(Win1250Converter)
+	in := []byte{0xC4, 0xF5}
+	b, err := dec.Decode(in)
+	if err != nil {
+		t.Fatalf("error in decode: %s", err)
+	}
+	want := "Äő"
+	if string(b) != want {
+		t.Errorf("Want %s, have %s", want, string(b))
+	}
+}
+
+func TestWin1250Converter_Encode(t *testing.T) {
+	c := new(Win1250Converter)
+	in := []byte("Äő")
+	want := []byte{0xC4, 0xF5}
+
+	b, err := c.Encode(in)
+	if err != nil {
+		t.Fatalf("error in encode: %s", err)
+	}
+
+	if string(want) != string(b) {
+		t.Errorf("Want %s, have %s", want, string(b))
+	}
+}
