0c62b654492259652a7ebd330c3ec89074e9435e
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2022-06-22 21:33:38 +0200

    [FIX] moved constants

--- dbase/constants.go
+++ dbase/constants.go
@@ -14,10 +14,27 @@ const (
 	// returned when an invalid column position is used (x<1 or x>number of columns)
 	ERROR_INVALID DBaseError = "INVALID"
 	// returned when a file operation is attempted on a non existent file
-	ERROR_NO_DBF_FILE DBaseError = "FPT_FILE_NOT_FOUND"
-	ERROR_NO_FPT_FILE DBaseError = "DBF_FILE_NOT_FOUND"
-
+	ERROR_NO_DBF_FILE      DBaseError = "FPT_FILE_NOT_FOUND"
+	ERROR_NO_FPT_FILE      DBaseError = "DBF_FILE_NOT_FOUND"
 	ERROR_INVALID_ENCODING DBaseError = "INVALID_ENCODING"
+
+	// Supported file types
+	FOXPRO               byte = 0x30
+	FOXPRO_AUTOINCREMENT byte = 0x31
+
+	// Relevant byte marker
+	NULL          byte = 0x00
+	BLANK         byte = 0x20
+	END_OF_COLUMN byte = 0x0D
+	ACTIVE             = BLANK
+	DELETED            = 0x2A
+	EOF_MARKER    byte = 0x1A
+
+	// dBase Table flags
+	STRUCTURAL      byte = 0x01
+	MEMO            byte = 0x02
+	STRUCTURAL_MEMO byte = 0x03
+	DATABASE        byte = 0x04
 )
 
 func (re DBaseError) AsError() error {
