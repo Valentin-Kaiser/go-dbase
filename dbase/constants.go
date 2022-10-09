fa084667d3f9fe313108cf4048715bbe1612e491
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2022-10-09 15:11:49 +0200

    [FIX] Error type name

--- dbase/constants.go
+++ dbase/constants.go
@@ -1,22 +1,5 @@
 package dbase
 
-import "errors"
-
-var (
-	// Returned when the end of a dBase database file is reached
-	ErrEOF error = errors.New("EOF")
-	// // Returned when the row pointer is attempted to be moved before the first row
-	ErrBOF error = errors.New("BOF")
-	// // Returned when the read of a row or column did not finish
-	ErrIncomplete error = errors.New("INCOMPLETE")
-	// // Returned when a file operation is attempted on a non existent file
-	ErrNoFPT error = errors.New("FPT_FILE_NOT_FOUND")
-	ErrNoDBF error = errors.New("DBF_FILE_NOT_FOUND")
-	// // Returned when an invalid column position is used (x<1 or x>number of columns)
-	ErrInvalidPosition error = errors.New("INVALID_Position")
-	ErrInvalidEncoding error = errors.New("INVALID_ENCODING")
-)
-
 const (
 	// Supported and testet file types - other file types may work but are not tested
 	// The file type check has to be bypassed when opening, if the file type is not supported
--- dbase/error.go
+++ dbase/error.go
@@ -1,21 +1,38 @@
 package dbase
 
-type DBaseError struct {
+import "errors"
+
+var (
+	// Returned when the end of a dBase database file is reached
+	ErrEOF = errors.New("EOF")
+	// // Returned when the row pointer is attempted to be moved before the first row
+	ErrBOF = errors.New("BOF")
+	// // Returned when the read of a row or column did not finish
+	ErrIncomplete = errors.New("INCOMPLETE")
+	// // Returned when a file operation is attempted on a non existent file
+	ErrNoFPT = errors.New("FPT_FILE_NOT_FOUND")
+	ErrNoDBF = errors.New("DBF_FILE_NOT_FOUND")
+	// // Returned when an invalid column position is used (x<1 or x>number of columns)
+	ErrInvalidPosition = errors.New("INVALID_Position")
+	ErrInvalidEncoding = errors.New("INVALID_ENCODING")
+)
+
+type Error struct {
 	context string
 	err     error
 }
 
-func newError(context string, err error) DBaseError {
-	return DBaseError{
+func newError(context string, err error) Error {
+	return Error{
 		context: context,
 		err:     err,
 	}
 }
 
-func (e DBaseError) Error() string {
+func (e Error) Error() string {
 	return e.err.Error()
 }
 
-func (e DBaseError) Context() string {
+func (e Error) Context() string {
 	return e.context
 }
