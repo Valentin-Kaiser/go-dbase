d6acf9c4b57ac162f88d23205b25d270f2db80f6
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2022-10-14 14:54:49 +0200

    [FIX] Create example

--- examples/create/create.go
+++ examples/create/create.go
@@ -13,25 +13,28 @@ func main() {
 		panic(err)
 	}
 
-	// Field name are always save uppercase
+	// Field name are always saved uppercase
 	nameCol, err := dbase.NewColumn("Name", dbase.Character, 20, 0, false)
 	if err != nil {
 		panic(err)
 	}
 
-	// Memo fields need no length the memo block size is defined when calling New()
+	// Memo fields need no length the memo block size is defined as last parameter when calling New()
 	memoCol, err := dbase.NewColumn("Memo", dbase.Memo, 0, 0, false)
 	if err != nil {
 		panic(err)
 	}
 
+	// Some fields can be null this is defined by the last parameter
 	varCol, err := dbase.NewColumn("Var", dbase.Varchar, 64, 0, true)
 	if err != nil {
 		panic(err)
 	}
 
+	// When creating a new table you need to define table type
+	// For more information about table types see the constants.go file
 	dbf, err := dbase.New(
-		0x32,
+		dbase.FoxProVar,
 		&dbase.Config{
 			Filename:   "test.dbf",
 			Converter:  new(dbase.Win1250Converter),
@@ -70,14 +73,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	err = row.FieldByName("NAME").SetValue("TOTALLY_NEW_ROW")
 	if err != nil {
 		panic(err)
 	}
+
 	err = row.FieldByName("MEMO").SetValue("This is a memo field")
 	if err != nil {
 		panic(err)
 	}
+
 	err = row.FieldByName("VAR").SetValue("This is a varchar field")
 	if err != nil {
 		panic(err)
