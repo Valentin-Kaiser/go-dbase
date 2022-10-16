d371b0acda26fe5100333346f7de4a58b1eb3db6
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2022-10-17 00:13:26 +0200

    [ADD] Constants

--- dbase/constants.go
+++ dbase/constants.go
@@ -1,42 +1,43 @@
 package dbase
 
-// Supported and testet file types - other file types may work but are not tested
-// The file type check has to be bypassed when opening a file type that is not supported
+// Supported and testet file versions - other files may work but are not tested
+// The file version check has to be bypassed when opening a file type that is not supported
 // https://learn.microsoft.com/en-us/previous-versions/visualstudio/foxpro/st4a0s68(v=vs.71)
-type FileType byte
+type FileVersion byte
 
 // Supported and testet file types - other file types may work but are not tested
 const (
-	FoxPro              FileType = 0x30
-	FoxProAutoincrement FileType = 0x31
-	FoxProVar           FileType = 0x32
+	FoxPro              FileVersion = 0x30
+	FoxProAutoincrement FileVersion = 0x31
+	FoxProVar           FileVersion = 0x32
 )
 
 // Not tested
 const (
-	FoxBase         FileType = 0x02
-	FoxBase2        FileType = 0xFB
-	FoxBasePlus     FileType = 0x03
-	DBaseSQLTable   FileType = 0x43
-	FoxBasePlusMemo FileType = 0x83
-	DBaseMemo       FileType = 0x8B
-	DBaseSQLMemo    FileType = 0xCB
-	FoxPro2Memo     FileType = 0xF5
+	FoxBase         FileVersion = 0x02
+	FoxBase2        FileVersion = 0xFB
+	FoxBasePlus     FileVersion = 0x03
+	DBaseSQLTable   FileVersion = 0x43
+	FoxBasePlusMemo FileVersion = 0x83
+	DBaseMemo       FileVersion = 0x8B
+	DBaseSQLMemo    FileVersion = 0xCB
+	FoxPro2Memo     FileVersion = 0xF5
 )
 
 // Table file extenstions
 type FileExtension string
 
 const (
-	DatabaseFileExtension          FileExtension = ".DBC"
-	TableFileExtension             FileExtension = ".DBT"
-	MemoFileExtension              FileExtension = ".FPT"
-	FormFileExtension              FileExtension = ".SCX"
-	LabelFileExtension             FileExtension = ".LBX"
-	MenuFileExtension              FileExtension = ".MNX"
-	ProjectFileExtension           FileExtension = ".PJX"
-	ReportFileExtension            FileExtension = ".RPX"
-	VisualClassLibaryFileExtension FileExtension = ".VCX"
+	DBC FileExtension = ".DBC" // Database file extension
+	DCT FileExtension = ".DCT" // Database container file extension
+	DBT FileExtension = ".DBT" // Table file extension
+	FPT FileExtension = ".FPT" // Memo file extension
+	SCX FileExtension = ".SCX" // Form file extension
+	LBX FileExtension = ".LBX" // Label file extension
+	MNX FileExtension = ".MNX" // Menu file extension
+	PJX FileExtension = ".PJX" // Project file extension
+	RPX FileExtension = ".RPX" // Report file extension
+	VCX FileExtension = ".VCX" // Visual class library file extension
 )
 
 // Important byte marker for the dbase file
@@ -79,21 +80,21 @@ const (
 type DataType byte
 
 const (
-	Character DataType = 0x43
-	Currency  DataType = 0x59
-	Double    DataType = 0x42
-	Date      DataType = 0x44
-	DateTime  DataType = 0x54
-	Float     DataType = 0x46
-	Integer   DataType = 0x49
-	Logical   DataType = 0x4C
-	Memo      DataType = 0x4D
-	Numeric   DataType = 0x4E
-	Blob      DataType = 0x57
-	General   DataType = 0x47
-	Picture   DataType = 0x50
-	Varbinary DataType = 0x51
-	Varchar   DataType = 0x56
+	Character DataType = 0x43 // C - Character (string)
+	Currency  DataType = 0x59 // Y - Currency (float64)
+	Double    DataType = 0x42 // B - Double (float64)
+	Date      DataType = 0x44 // D - Date (time.Time)
+	DateTime  DataType = 0x54 // T - DateTime (time.Time)
+	Float     DataType = 0x46 // F - Float (float64)
+	Integer   DataType = 0x49 // I - Integer (int32)
+	Logical   DataType = 0x4C // L - Logical (bool)
+	Memo      DataType = 0x4D // M - Memo (string)
+	Numeric   DataType = 0x4E // N - Numeric (int64)
+	Blob      DataType = 0x57 // W - Blob ([]byte)
+	General   DataType = 0x47 // G - General (string)
+	Picture   DataType = 0x50 // P - Picture (string)
+	Varbinary DataType = 0x51 // Q - Varbinary ([]byte)
+	Varchar   DataType = 0x56 // V - Varchar (string)
 )
 
 // Returns the type of the column as string
