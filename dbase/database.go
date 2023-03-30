a400b708a1d12daec4726d68e8f87d04664230ee
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2023-03-30 20:37:48 +0200

    [FIX] Indentation

--- dbase/database.go
+++ dbase/database.go
@@ -66,7 +66,7 @@ func OpenDatabase(config *Config) (*Database, error) {
 			Untested:                          config.Untested,
 			TrimSpaces:                        config.TrimSpaces,
 			DisableConvertFilenameUnderscores: config.DisableConvertFilenameUnderscores,
-      ReadOnly:                          config.ReadOnly,
+			ReadOnly:                          config.ReadOnly,
 			WriteLock:                         config.WriteLock,
 			ValidateCodePage:                  config.ValidateCodePage,
 			InterpretCodePage:                 config.InterpretCodePage,
